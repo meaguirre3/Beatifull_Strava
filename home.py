944f9882c29b5c98499566313c6db2c5d53b2990
Marco Aguirre <meaguirre3@gmail.com>
2024-02-19 21:28:08 -0500

    Update home.py

--- home.py
+++ home.py
@@ -5,12 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import util as ut
 import osmnx as ox
+import io
+from PIL import Image
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.title(' Beatifull Strava')
 
-uploaded_file = st.file_uploader("Choose a Strava Route GPX",type=['gpx'])
+uploaded_file = st.file_uploader("Choose a Strava Route GPX",type=['gpx'],accept_multiple_files=False)
 
 col1, col2, col3 , col4 ,col5= st.columns(5)
 
@@ -40,7 +42,7 @@ with col4:
    colorText   = st.color_picker('Text Color', '#00f900', on_change = on_color_change)
 
 with col5:
-   title  = st.text_input('Title of Route', '')
+   title  = st.text_input('Title of Route', 'Title',on_change = on_color_change )
 
 # Crea una figura 
 
@@ -60,3 +62,26 @@ if uploaded_file is not None:
     # To read file as bytes:
    read_route(uploaded_file)
 
+name_file  = title+".png"
+# Create the download button
+
+# Convert the figure to different image formats
+def fig_to_bytes(fig, format='png'):
+    buf = io.BytesIO()
+    fig.savefig(buf, format=format)
+    buf.seek(0)
+    return buf.getvalue()
+
+
+if uploaded_file is not None:
+   
+   name_file  = title+".png"
+# Assuming you have a figure 'fig' already defined
+   png_data = fig_to_bytes(fig, format='png')
+
+   btn = st.download_button(
+      label="Download image",
+      data=png_data,
+      file_name=name_file,
+      mime="image/png"
+   )
