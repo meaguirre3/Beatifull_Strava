7b39e10a2a90daac6ca6fdb4fe40381412e625af
Marco Aguirre <meaguirre3@gmail.com>
2024-02-19 20:07:44 -0500

    Update home.py

--- home.py
+++ home.py
@@ -6,67 +6,57 @@ import numpy as np
 import util as ut
 import osmnx as ox
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.title(' Beatifull Strava')
+
 uploaded_file = st.file_uploader("Choose a Strava Route GPX",type=['gpx'])
 
 col1, col2, col3 , col4 ,col5= st.columns(5)
 
+figure = st.pyplot()
+fig = None
+def update_fig():
+   global fig
+   fig = ut.plot_figure(global_variable , route_df ,colorBackground , 
+                         colorLines,colorRoute,title,colorText )
+   figure.pyplot(fig)
+
+def on_color_change():
+    if uploaded_file is not None:
+      update_fig()
+
 with col1:
-   colorBackground   = st.color_picker('Background Color', "#415DC0")
+   colorBackground   = st.color_picker('Background Color', "#06152A", on_change = on_color_change)
+   #st.session_state.prev_color = colorBackground
 
 with col2:
-   colorLines   = st.color_picker('Lines Color', "#9DA5D9")
+   colorLines   = st.color_picker('Lines Color', "#9DA5D9", on_change = on_color_change)
 
 with col3:
-   colorRoute  = st.color_picker('Route Color', "#E64E25")
+   colorRoute  = st.color_picker('Route Color', "#E64E25", on_change = on_color_change)
 
 with col4:
-   colorText   = st.color_picker('Text Color', '#00f900')
+   colorText   = st.color_picker('Text Color', '#00f900', on_change = on_color_change)
 
 with col5:
-   title  = st.text_input('Title of Figure', 'title')
+   title  = st.text_input('Title of Route', '')
 
-button = st.button("Update Map", type="primary")
-
-
-
-# Crea una figura sin ejes
+# Crea una figura 
 
 route_df = None
 
 def read_route(file):
     global route_df
-    route_df = ut.read_gpx_file(file)
-    c        = ut.find_centroid(route_df)
-    d        =  ut.get_dist_graph(route_df ,  c)
-    elevation = route_df.elevation
-    x_plot    = range(len(elevation))
-    min_elev  = min(elevation) - 25
-    G = ox.graph_from_point(c, dist=d, retain_all=True, simplify = True, network_type='bike')
+    route_df  = ut.read_gpx_file(file)
+    c         = ut.find_centroid(route_df)
+    d         = ut.get_dist_graph(route_df ,  c)
+    G         = ox.graph_from_point(c, dist=d, retain_all=True, simplify = True, network_type='bike')
     global global_variable
     global_variable = G
-
-
-
+    update_fig()
 
 if uploaded_file is not None:
     # To read file as bytes:
    read_route(uploaded_file)
 
-if button:
-    fig, ax = ox.plot_graph(global_variable, node_size=0,
-                        figsize        = (27, 40), 
-                        dpi            = 300,
-                        save           = False,
-                        bgcolor        = colorBackground,
-                        edge_color     = colorLines,
-                        edge_alpha     = 1 ,
-                        show           = False)
-    
-## Plot  activity in graph 
-    ax.plot( route_df['longitude'] , route_df['latitude'] ,
-               color     = colorRoute , 
-               linewidth = 4.0)
-      #bbox={'facecolor': '#415DC0', 'edgecolor': '#415DC0', 'pad': 5}
-    ax.text(0.5, 0.95, title, horizontalalignment='center',color = colorText,
-         verticalalignment='top', transform=ax.transAxes, fontsize=150)
-    st.pyplot(fig)
